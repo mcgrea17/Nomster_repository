c514930763cf698ddd17e9d29affdaaa2506af49
Amber McGregor <amber@vqhomes.com>
2019-04-18 20:57:39 +0000

    Adding email notifications2

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: 'no-reply@nomsterapp.com'
 
   def comment_added
-    mail(to: "amber@vqhomes.com",
+    mail(to: "amberlmcgregor17@gmail.com",
           subject: "A comment has been added to your place")
   end
 end

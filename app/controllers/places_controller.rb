5f50434496617a6c92e9345baaa1fa792730ec0b
Amber McGregor <amber@vqhomes.com>
2019-04-18 14:53:35 +0000

    Added Images

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -34,6 +34,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @photo = Photo.new
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_172056) do
+ActiveRecord::Schema.define(version: 2019_04_17_221555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_04_17_172056) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["place_id"], name: "index_photos_on_place_id"
   end
 
